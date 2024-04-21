1a64c52d4cf2e58345792d6639f8ff62f58a19b7
Kennn <111826360+Principiante12344@users.noreply.github.com>
2024-04-21 00:34:25 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -430,7 +430,7 @@ global.dfail = (type, m, conn) => {
         private: '• Esta función solo se puede usar en el chat *privado* de la bot',
         admin: '• Esta función es solo para *admins* del grupo',
         botAdmin: '• Para ejecutar esta función debo ser *administradora*',
-        unreg: 'Regístrese para usar esta función  Escribiendo:\n\n*/reg nombre.edad*\n\n🍭 Ejemplo : */reg おDaniel.666*',
+        unreg: 'Regístrese para usar esta función  Escribiendo:\n\n*/reg nombre.edad*\n\n🍭 Ejemplo : */reg kenn.20*',
         restrict: '• Esta función está *deshabilitada*',
         nsfw: '• En este grupo está prohibido el contenido +18'
     }[type]
